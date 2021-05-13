d655168b0b5bc48774c1bb2c1dd95e9ea7590470
mehak sohal <mehak.sohal94@gmail.com>
2021-05-13 17:24:40 -0400

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Logo src="/images/logo.svg" alt="" />
+      <NavItems>
+        <Wrap>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>Home</span>
+        </Wrap>
+        <Wrap>
+          <img src="/images/search-icon.svg" alt="" />
+          <span>Search</span>
+        </Wrap>
+        <Wrap>
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>watchlist</span>
+        </Wrap>
+        <Wrap>
+          <img src="/images/original-icon.svg" alt="" />
+          <span>originals</span>
+        </Wrap>
+        <Wrap>
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>movies</span>
+        </Wrap>
+        <Wrap>
+          <img src="/images/series-icon.svg" alt="" />
+          <span>series</span>
+        </Wrap>
+      </NavItems>
+      <Avatar src="/images/avatar-default.png" alt="" />
+    </Container>
+  );
+};
+
+export default Navbar;
+
+const Container = styled.div`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  background-color: #000;
+`;
+
+const Logo = styled.img`
+  height: 6vh;
+  object-fit: contain;
+  margin: 0px 8px;
+  cursor: pointer;
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 4px;
+  padding: 0 8px;
+  font-size: 12px;
+  cursor: pointer;
+
+  img {
+    height: 3vh;
+    margin-right: 4px;
+  }
+
+  span {
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+    font-weight: 400;
+    position: relative;
+
+    &:after {
+      content: "";
+      position: absolute;
+      height: 2px;
+      background: #fff;
+      left: 0;
+      right: 0;
+      bottom: -4px;
+      opacity: 0;
+      transform-origin: left center;
+      transition: all 250ms cubic-bezier(0.25, 0.45, 0.45, 0.95) 0s;
+    }
+  }
+
+  &:hover {
+    span:after {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+`;
+
+const Avatar = styled.img`
+  height: 6vh;
+  border-radius: 50%;
+  margin-right: 8px;
+  cursor: pointer;
+`;
